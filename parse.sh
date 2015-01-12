9f9baa236c2b9a2e45cf3981a93e63d7b47a43b3
Ivan Shapovalov <intelfx100@gmail.com>
2015-01-12 13:55:55 +0300

    Add script for parsing the raw interview source

---
+++ parse.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+STATE=q
+CONT=
+
+COUNT=0
+
+rm -r qa-*.txt
+
+while read line; do
+	case "$STATE" in
+	q)
+		if ! [[ "$line" ]]; then continue; fi
+
+		if [[ "$line" == "[–]lennart-poettering"* ]]; then
+			echo ""
+			STATE=a
+			CONT=
+		else
+			if ! (( CONT )); then
+				exec > parsed/qa-$(( ++COUNT )).txt
+				echo "Q:"
+				CONT=1
+			fi
+
+			echo "$line"
+		fi
+		;;
+
+	a)
+		if [[ "$line" == "----"* ]] || [[ "$line" == "===="* ]]; then
+			echo ""
+			echo "----"
+			echo ""
+			STATE=tr_q
+			CONT=
+		else
+			if ! (( CONT )); then
+				echo "A:"
+				CONT=1
+			fi
+
+			echo "$line"
+		fi
+		;;
+
+	tr_q)
+		echo "Q:"
+		echo "$line"
+		echo ""
+		STATE=tr_a
+		;;
+
+	tr_a)
+		if [[ -z "$line" ]]; then
+			echo ""
+			STATE=q
+			CONT=
+		else
+			if ! (( CONT )); then
+				echo "A:"
+				CONT=1
+			fi
+
+			echo "$line"
+		fi
+		;;
+
+	*)
+		echo "WRONG STATE: $STATE" >&2
+		exit 1
+	esac
+done
